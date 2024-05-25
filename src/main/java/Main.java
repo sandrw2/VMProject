c5a56b6e4d14f966686ebd832260a3f8436054e8
sandrw2 <sandrawang56@gmail.com>
2024-05-24 21:56:43 -0700

    Fixed translate VA math logic and got everything running properly with sample input txt

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,69 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) {
+        //create output file
+        String filePath = "output.txt";
+        BufferedWriter writer = createWriter(filePath);
+
         vmBasic basicVM = new vmBasic();
-        String filePath = "basicInit.txt";
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))) {
+        String initFilePath = "sampleBasicInit.txt";
+        String commandFilePath = "sampleBasicCommand.txt";
+        try {
+            // Initialize PM
+            BufferedReader initBufferedReader = new BufferedReader(new FileReader(initFilePath));
             String line;
             String[] splitLine;
-            line = bufferedReader.readLine();
+            line = initBufferedReader.readLine();
             splitLine = line.split(" ");
             List<String> ST = Arrays.stream(splitLine).toList();
-            line = bufferedReader.readLine();
+
+            line = initBufferedReader.readLine();
             splitLine = line.split(" ");
             List<String> PT = Arrays.stream(splitLine).toList();
             basicVM.init(ST,PT);
+
+            // Read Commands
+            BufferedReader commandBufferedReader = new BufferedReader(new FileReader(commandFilePath));
+            while ((line = commandBufferedReader.readLine()) != null) {
+                splitLine = line.split(" ");
+
+                if(splitLine[0].equals("TA")){
+                    int VA = Integer.parseInt(splitLine[1]);
+                    String PA = String.valueOf(basicVM.translateVA(VA));
+                    writer.write(PA + " ");
+                    writer.flush();
+                }else if(splitLine[0].equals("RP")){
+                    int PA = Integer.parseInt(splitLine[1]);
+                    String val = String.valueOf(basicVM.readPA(PA));
+                    writer.write(val + " ");
+                    writer.flush();
+                }else if(splitLine[0].equals("NL")){
+                    writer.newLine();
+                    writer.flush();
+                }
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+    }
+
+    public static BufferedWriter createWriter(String filePath){
+        File file = new File(filePath);
+        try {
+            if (!file.exists()) {
+                // Create a new file
+                file.createNewFile();
+            }
+            return new BufferedWriter(new FileWriter(file));
+        } catch (IOException e) {
+            System.out.println("Could not create writer");
+            return null;
         }
     }
 }
--- src/main/java/vmBasic.java
+++ src/main/java/vmBasic.java
@@ -1,58 +1,78 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class vmBasic {
-    //PM size = 524,288 integers
-    ArrayList<Integer> PM;
-    public vmBasic(){
 
+public class vmBasic {
+    static final boolean DEBUG = true;
+            //PM size = 524,288 integers
+    Integer[] PM;
+    public vmBasic(){
+        PM = new Integer[524288];
+        Arrays.fill(PM, 0);
     }
     public void init(List<String> ST, List<String> PT) {
-        for(int i = 0; i < ST.size()-3; i++){
+
+        for(int i = 0; i <= ST.size()-3; i+=3){
             int s = Integer.parseInt(ST.get(i));
             int z = Integer.parseInt(ST.get(i+1));
             int f = Integer.parseInt(ST.get(i+2));
             //PM[2s] = z (segment size)
-            PM.add(2*s, z);
+            PM[2*s] = z;
             //PM[2s+1] = f (frame size)
-            PM.add((2*s)+1, f);
+            PM[2*s+1] = f;
+            if(DEBUG){
+                String message = String.format("SegmentSize: %d, FrameSize: %d", PM[2*s], PM[2*s+1]);
+                System.out.println(message);
+            }
         }
-        for(int i = 0; i< PT.size()-3; i++){
+        for(int i = 0; i<= PT.size()-3; i+=3){
             int s = Integer.parseInt(PT.get(i));
             int p = Integer.parseInt(PT.get(i+1));
             int f = Integer.parseInt(PT.get(i+2));
             //PM[PM[2s+1]*512+p] = f
-            int location = (PM.get((2*s)+1)*512)+p;
-            PM.add((PM.get((2*s)+1)*512)+p, f);
+            int location = PM[2*s+1]*512+p;
+            PM[location] = f;
+            if(DEBUG){
+                String message = String.format("PageFrame: %d", PM[PM[2*s+1]*512+p]);
+                System.out.println(message);
+            }
         }
+
+
     }
 
-    public int vaTranslate(int va){
+    public int translateVA(int va){
         int s = getS(va);
         int w = getW(va);
         int p = getP(va);
         int pw = getPW(va);
+
+        if(DEBUG){
+            String message = String.format("s:%d, w:%d, p:%d, pw:%d, ST_size: %d", s, w, p, pw, PM[2*s]);
+            System.out.print(message);
+        }
         //ST size = PM[2s]
-        int ST_size = PM.get(2*s);
+        int ST_size = PM[2*s];
         if(pw >= ST_size){
             return -1;
         }
         //ST frame  = PM[2s+1]
-        int ST_frame = PM.get((2*s)+1);
+        int ST_frame = PM[(2*s)+1];
         //ST address = PM[2s+1]*512
         int ST_address = ST_frame*512;
         //page frame = PM[PM[2s+1]*512+p]
-        int page_frame = PM.get(ST_address+p);
+        int page_frame = PM[ST_address+p];
         //page address = PM[PM[2s+1]*512+p]*512
-        int page_address = PM.get(page_frame*512);
+        int page_address = page_frame*512;
         //PA = PM[PM[2s+1]*512+p]*512 + w
         return page_address + w;
 
      }
+
+    public int readPA(int PA){
+        return PM[PA];
+    }
     public int getS(int va){
         return va >>> 18;
     }
